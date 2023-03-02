44374d2219b3985e000c39a5c4d091078da0b3cb
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-03-02 20:00:28 +0100

    the_csv.py

--- the_csv.py
+++ the_csv.py
@@ -1,18 +1,23 @@
-#1. Odczytać pliku csv z zapisem poszczególnych pól
-
+1.odczytac plik csv z zapisem poszczegolnych pol
 path = 'C:\\Users\\vdi-student\\Desktop\\first_repo\\rozliczenie.csv'
 mode = 'r'
 with open (path, mode) as plik_csv:
-    content = plik_csv.readlines()
+	content = plik_csv.readlines()
+for i in range(len(content)):
+	content[i] = content[i].replace('\n', '')
+	content[i] = content[i].split(';')
+#2. obliczenie sredniej wyplaty
+total = 0
+for i in range(1, len(content)):
 
-print(type(content))
-print(len(content))
-print(content)
-print(content[4])
+	total += int(content)):
+average = total/len(content)-1)
+print('Srednia wynosi', round(average, 2), 'zlotych')
+print(f'Srednia wynosi {round(average, 2)} zlotych')
 
-dane = 'mama.tata.pies'
-print(dane)
-dane2 = dane.split('.')
-print(dane2)
-
-#with open('C:\\Users\\vdi-student\\Desktop\\first_repo\\rozliczenie.csv', 'r') as plik_csv:
+#3. obliczenie liczby kobieta na macierzynskim
+total = 0
+for i in range(1, len(content)):
+	if content[i][4] == 't' and content[i][3] == 'k':
+		total +=1
+print(total)
