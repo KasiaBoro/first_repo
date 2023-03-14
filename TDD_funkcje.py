de5e21ea8c8519e54e2d4c1c5bb03f4bea8563fb
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-03-14 20:00:34 +0100

    tdd test

--- TDD_funkcje.py
+++ TDD_funkcje.py
@@ -2,11 +2,12 @@ Def przywitanie(imie, nazwisko, wiek):
 	print(‘siema’, imie)
 	If wiek >=18:
 		print(‘Szanowny’, nazwisko)
-x = input(‘Podaj imie, nazwisko i wiek – oddziel spacja’).split()
+x = input(‘ Podaj imie, nazwisko i wiek – oddziel spacja ’).split()
 
 przywitanie(x[0], x[1], int(x[2]))
 #while true:
 #	pin = ‘1234’
+# while True:
 #	x = input(‘Wprowadz pin’)
 #	if x ==pin:
 #	przywitanie()
---
+++ tdd_tests.py
@@ -0,0 +1,17 @@
+from functions import *
+
+# def test_add():
+#    assert add(3, 5) == 8
+#     assert add(120, 54) == 174
+#
+# def test_multiply():
+#     assert multiply(100, 5) == 5000
+#     assert multiply(100, 1.1) == 110
+
+def test_fissbuzz():
+    assert fissbuzz(1) == 1
+    assert fissbuzz(2) == 2
+    assert fissbuzz(3) == 'fiss'
+    assert fissbuzz(5) == 'buzz'
+
+
