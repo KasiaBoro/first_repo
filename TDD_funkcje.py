fe4deb0104857c90d8523e926ce26a01c9e3d1b1
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-03-14 19:11:40 +0100

    TDD.Funkcje.py

---
+++ TDD_funkcje.py
@@ -0,0 +1,19 @@
+Def przywitanie(imie, nazwisko, wiek):
+	print(‘siema’, imie)
+	If wiek >=18:
+		print(‘Szanowny’, nazwisko)
+x = input(‘Podaj imie, nazwisko i wiek – oddziel spacja’).split()
+
+przywitanie(x[0], x[1], int(x[2]))
+#while true:
+#	pin = ‘1234’
+#	x = input(‘Wprowadz pin’)
+#	if x ==pin:
+#	przywitanie()
+#	break
+#print(‘Kolejna instrukcja’)
+
+#x = 0
+#while x < 5:
+#	print(‘Jakas akcja’)
+#	x +=1
